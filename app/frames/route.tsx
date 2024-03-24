52436e314953ae081751840a3ac460f997e49fcc
TejasAnand <anandtejas455@gmail.com>
2024-03-24 03:36:00 -0400

    first placeholder

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -9,16 +9,15 @@ const frames = createFrames({
   basePath: "/frames",
 });
 
-  const handleRequest = frames(async (ctx) => {
+const handleRequest = frames(async (ctx) => {
   const pageIndex = Number(ctx.searchParams.pageIndex || 0);
   const rouletteOutcome = ctx.searchParams.rouletteOutcome; // This could be 'win', 'lose', or undefined
   const images = [
     {
-      src: "https://tableforchange.com/wp-content/uploads/2020/08/1_1431500821-1.jpg"
+      src: "https://tableforchange.com/wp-content/uploads/2020/08/1_1431500821-1.jpg",
     },
     {
-      src: "https://assets-global.website-files.com/6344c9cef89d6f2270a38908/658620295b0868b3ba611370_What%20is%20a%20Blockchain%20Developer%20A%2B%20Guide%20For%20Hiring%20in%202024.webp"
-      
+      src: "https://assets-global.website-files.com/6344c9cef89d6f2270a38908/658620295b0868b3ba611370_What%20is%20a%20Blockchain%20Developer%20A%2B%20Guide%20For%20Hiring%20in%202024.webp",
     },
     {
       src: "https://remote-image.decentralized-content.com/image?url=https%3A%2F%2Fgifyu.com%2Fimage%2FSVKbw&w=1920&q=75",
@@ -30,7 +29,7 @@ const frames = createFrames({
 
   if (pageIndex == 0) {
     pageContent = "Your Cards";
-    inputField = "Your cards";
+    inputField = "Choose: Blue Pill or Red Pill";
   } else if (pageIndex == 1) {
     inputField = "Higher Lower";
     if (rouletteOutcome === "win") {
@@ -42,16 +41,13 @@ const frames = createFrames({
     pageContent = "ROULETTE";
   }
 
-  
-
   return {
-    image: images[pageIndex % images.length]?.src || 'default_image_url',
+    image: images[pageIndex % images.length]?.src || "default_image_url",
     imageOptions: {
-    aspectRatio: "1:1",
-    overlaytext: pageContent,
-  },
-    text: pageContent  
-    ,
+      aspectRatio: "1:1",
+      overlaytext: pageContent,
+    },
+    text: pageContent,
     buttons: [
       // eslint-disable-next-line react/jsx-key
       <Button
