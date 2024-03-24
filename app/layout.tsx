bd474751c13d19e4df4d0251b6ed1513a697bc88
TejasAnand <anandtejas455@gmail.com>
2024-03-24 07:47:18 -0400

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Providers from "./components/providers";
 export const metadata: Metadata = {
   // without a title, warpcast won't validate your frame
   title: "frames.js starter",
@@ -13,10 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <Providers>
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    </Providers>
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   );
 }
