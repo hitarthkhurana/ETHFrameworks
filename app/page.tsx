35b8ef8ec4d9ffd3a63a29953f68b614f71ad94a
TejasAnand <anandtejas455@gmail.com>
2024-03-23 21:31:31 -0400

    multi-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,33 @@
-import {
-  FrameButton,
-  FrameContainer,
-  FrameImage,
-  FrameReducer,
-  NextServerPageProps,
-  getPreviousFrame,
-  useFramesReducer,
-} from "frames.js/next/server";
 import Link from "next/link";
-import { currentURL } from "./utils";
+import { currentURL, vercelURL } from "./utils";
 import { createDebugUrl } from "./debug";
+import type { Metadata } from "next";
+import { fetchMetadata } from "frames.js/next";
 
-type State = {
-  pageIndex: number;
-};
-
-const totalPages = 5;
-const initialState: State = { pageIndex: 0 };
-
-const reducer: FrameReducer<State> = (state, action) => {
-  const buttonIndex = action.postBody?.untrustedData.buttonIndex;
-
+export async function generateMetadata(): Promise<Metadata> {
   return {
-    pageIndex: buttonIndex
-      ? (state.pageIndex + (buttonIndex === 2 ? 1 : -1)) % totalPages
-      : state.pageIndex,
+    title: "New api example",
+    description: "This is a new api example",
+    other: {
+      ...(await fetchMetadata(
+        new URL(
+          "/examples/new-api-multi-page/frames",
+          vercelURL() || "http://localhost:3000"
+        )
+      )),
+    },
   };
-};
+}
 
-// This is a react server component only
-export default async function Home({ searchParams }: NextServerPageProps) {
-  const url = currentURL("/examples/multi-page");
-  const previousFrame = getPreviousFrame<State>(searchParams);
-  const [state] = useFramesReducer<State>(reducer, initialState, previousFrame);
-  const imageUrl = `https://picsum.photos/seed/frames.js-${state.pageIndex}/1146/600`;
+export default async function Home() {
+  const url = currentURL("/examples/new-api-multi-page");
 
-  // then, when done, return next frame
   return (
     <div>
-      Multi-page example <Link href={createDebugUrl(url)}>Debug</Link>
-      <FrameContainer
-        pathname="/examples/multi-page"
-        postUrl="/examples/multi-page/frames"
-        state={state}
-        previousFrame={previousFrame}
-      >
-        <FrameImage src="https://media.istockphoto.com/id/1272564863/photo/dark-web-browser-close-up-on-lcd-screen-with-shallow-focus-on-https-padlock.jpg?s=612x612&w=0&k=20&c=zIB-jYrqTnsbckMpTwrJSAVwfUdZvBo2g1p_86fKJFM=">
-          <div tw="flex flex-col">
-            <div tw="flex">
-              This is slide {state.pageIndex + 1} / {totalPages}
-            </div>
-          </div>
-        </FrameImage>
-        <FrameButton>←</FrameButton>
-        <FrameButton>→</FrameButton>
-      </FrameContainer>
+      Multi-page example
+      <Link href={createDebugUrl(url)} className="underline">
+        Debug
+      </Link>
     </div>
   );
 }
