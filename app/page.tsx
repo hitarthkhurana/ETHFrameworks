cc0602bb6d18d4b988dfb80d4f0dfa3e4214bffe
TejasAnand <anandtejas455@gmail.com>
2024-03-23 20:43:09 -0400

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,123 +2,58 @@ import {
   FrameButton,
   FrameContainer,
   FrameImage,
-  FrameInput,
   FrameReducer,
   NextServerPageProps,
-  getFrameMessage,
   getPreviousFrame,
   useFramesReducer,
 } from "frames.js/next/server";
 import Link from "next/link";
-import { DEFAULT_DEBUGGER_HUB_URL, createDebugUrl } from "./debug";
 import { currentURL } from "./utils";
-import { Button } from "frames.js/core";
+import { createDebugUrl } from "./debug";
 
 type State = {
-  active: string;
-  total_button_presses: number;
+  pageIndex: number;
 };
 
-const initialState = { active: "1", total_button_presses: 0 };
+const totalPages = 5;
+const initialState: State = { pageIndex: 0 };
 
 const reducer: FrameReducer<State> = (state, action) => {
+  const buttonIndex = action.postBody?.untrustedData.buttonIndex;
+
   return {
-    total_button_presses: state.total_button_presses + 1,
-    active: action.postBody?.untrustedData.buttonIndex
-      ? String(action.postBody?.untrustedData.buttonIndex)
-      : "1",
+    pageIndex: buttonIndex
+      ? (state.pageIndex + (buttonIndex === 2 ? 1 : -1)) % totalPages
+      : state.pageIndex,
   };
 };
 
 // This is a react server component only
 export default async function Home({ searchParams }: NextServerPageProps) {
-  const url = currentURL("/");
+  const url = currentURL("/examples/multi-page");
   const previousFrame = getPreviousFrame<State>(searchParams);
-
-  const frameMessage = await getFrameMessage(previousFrame.postBody, {
-    hubHttpUrl: DEFAULT_DEBUGGER_HUB_URL,
-  });
-
-  if (frameMessage && !frameMessage?.isValid) {
-    throw new Error("Invalid frame payload");
-  }
-
-  const [state, dispatch] = useFramesReducer<State>(
-    reducer,
-    initialState,
-    previousFrame
-  );
-
-  // Here: do a server side side effect either sync or async (using await), such as minting an NFT if you want.
-  // example: load the users credentials & check they have an NFT
-
-  console.log("info: state is:", state);
+  const [state] = useFramesReducer<State>(reducer, initialState, previousFrame);
+  const imageUrl = `https://picsum.photos/seed/frames.js-${state.pageIndex}/1146/600`;
 
   // then, when done, return next frame
   return (
-    <div className="p-4">
-      frames.js starter kit. The Template Frame is on this page, it&apos;s in
-      the html meta tags (inspect source).{" "}
-      <Link href={createDebugUrl(url)} className="underline">
-        Debug
-      </Link>{" "}
-      or see{" "}
-      <Link href="/examples" className="underline">
-        other examples
-      </Link>
+    <div>
+      Multi-page example <Link href={createDebugUrl(url)}>Debug</Link>
       <FrameContainer
-        postUrl="/frames"
-        pathname="/"
+        pathname="/examples/multi-page"
+        postUrl="/examples/multi-page/frames"
         state={state}
         previousFrame={previousFrame}
       >
-        <FrameImage src="/casino.jpeg" />
-        {/* <FrameImage aspectRatio="1.91:1">
-          <div tw="w-full h-full bg-purple-700 text-black justify-center items-center flex flex-col">
-            <div tw="flex flex-row">
-              {frameMessage?.inputText
-                ? frameMessage.inputText
-                : "WELCOME TO FRAMEWORKS CASINO !!!"}
-              <Button action="post">Click me</Button>
+        <FrameImage src="https://media.istockphoto.com/id/1272564863/photo/dark-web-browser-close-up-on-lcd-screen-with-shallow-focus-on-https-padlock.jpg?s=612x612&w=0&k=20&c=zIB-jYrqTnsbckMpTwrJSAVwfUdZvBo2g1p_86fKJFM=">
+          <div tw="flex flex-col">
+            <div tw="flex">
+              This is slide {state.pageIndex + 1} / {totalPages}
             </div>
-            {frameMessage && (
-              <div tw="flex flex-col">
-                <div tw="flex">
-                  Requester is @{frameMessage.requesterUserData?.username}{" "}
-                </div>
-                <div tw="flex">
-                  Requester follows caster:{" "}
-                  {frameMessage.requesterFollowsCaster ? "true" : "false"}
-                </div>
-                <div tw="flex">
-                  Caster follows requester:{" "}
-                  {frameMessage.casterFollowsRequester ? "true" : "false"}
-                </div>
-                <div tw="flex">
-                  Requester liked cast:{" "}
-                  {frameMessage.likedCast ? "true" : "false"}
-                </div>
-                <div tw="flex">
-                  Requester recasted cast:{" "}
-                  {frameMessage.recastedCast ? "true" : "false"}
-                </div>
-              </div>
-            )}
           </div>
-        </FrameImage> */}
-        <FrameInput text="TRY YOUR LUCK HERE!!!" />
-        <FrameButton>
-        {state?.active === "1" ? "High/Low" : "High/Low"}
-        </FrameButton>
-        <FrameButton>
-          {state?.active === "2" ? "Leaderboard" : "Leaderboard"}
-        </FrameButton>
-        <FrameButton
-          action="mint"
-          target={`eip155:7777777:0x060f3edd18c47f59bd23d063bbeb9aa4a8fec6df`}
-        >
-          Mint NFT
-        </FrameButton>
+        </FrameImage>
+        <FrameButton>←</FrameButton>
+        <FrameButton>→</FrameButton>
       </FrameContainer>
     </div>
   );
